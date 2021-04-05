b199fde282a59444deb3724deebd0f170806a267
Diana <dlicon2@miners.utep.edu>
2021-04-04 23:41:28 -0600

    red and green light bliink

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -2,43 +2,19 @@
 #include "stateMachines.h"
 #include "led.h"
 
-char toggle_red()		/* always toggle! */
-{
-  static char state = 0;
-
-  switch (state) {
-  case 0:
-    red_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
-}
-
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
-}
+			/* always change
 
 
 void state_advance()		/* alternate between toggling red & green */
+void state_advance()
 {
-  char changed = 0;  
-
-  static enum {R=0, G=1} color = G;
+  char changed=0;
+  static enum {R=0,G=1} color=G;
   switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
+  case R: green_on=1; red_on=0; color= G; break;
+  case G: red_on=1; green_on=0;color = R; break;
   }
+  changed=1;
 
   led_changed = changed;
   led_update();
